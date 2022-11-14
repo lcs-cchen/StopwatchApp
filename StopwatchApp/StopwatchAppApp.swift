9998021a326fdfc1c3b11b25910b4567d7f702aa
cyrus chen <pschen@lcs.on.ca>
2022-11-14 16:10:17 -0500

    Initial Commit

---
+++ StopwatchApp/StopwatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchAppApp.swift
+//  StopwatchApp
+//
+//  Created by Cyrus Chen on 14/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
