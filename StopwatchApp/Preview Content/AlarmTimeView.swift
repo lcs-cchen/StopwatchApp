85749fe6b5591191c94caf6cdf435bea1fb10552
cyrus chen <pschen@lcs.on.ca>
2022-11-18 09:22:39 -0500

    Added title to alarm

---
+++ StopwatchApp/Preview Content/AlarmTimeView.swift
@@ -0,0 +1,24 @@
+//
+//  AlarmTimeView.swift
+//  StopwatchApp
+//
+//  Created by Cyrus Chen on 18/11/2022.
+//
+
+import SwiftUI
+
+struct AlarmTimeView: View {
+  
+    
+    var body: some View {
+        Text("yes")
+        
+        
+    }
+}
+
+struct AlarmTimeView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlarmTimeView()
+    }
+}
---
+++ StopwatchApp/Preview Content/AlarmView.swift
@@ -0,0 +1,44 @@
+//
+//  AlarmView.swift
+//  StopwatchApp
+//
+//  Created by Cyrus Chen on 18/11/2022.
+//
+
+import SwiftUI
+
+struct AlarmView: View {
+    var body: some View {
+        VStack(alignment: .leading){
+            HStack {
+                Text("Edit")
+                    .foregroundColor(.orange)
+                    .padding(10)
+                Spacer()
+                Image(systemName: "plus")
+                    .foregroundColor(.orange)
+                    .padding(10)
+                
+            }
+            Text("Alarm")
+                .font(.largeTitle)
+                .bold()
+                .padding(10)
+            HStack{
+                Image(systemName: "bed.double.fill")
+                    .padding([.top, .leading, .bottom], 10)
+                Text("Sleep | Wake Up")
+                    .font(.title2)
+            }
+            HStack{
+                VStack{
+                    Text("No Alarm")
+                        .font(Font.system(size: 50))
+                    
+                    }
+                }
+        }
+    }
+}
+
+
